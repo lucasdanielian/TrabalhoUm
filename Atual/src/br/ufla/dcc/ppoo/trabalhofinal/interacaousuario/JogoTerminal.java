db7ae6200edb388a60cd6cc170003c77302ac77a
vssjunior <valdecisoares23@yahoo.com.br>
2017-07-07 16:31:12 -0300

    Correcao do jogo via terminal

--- Atual/src/br/ufla/dcc/ppoo/trabalhofinal/interacaousuario/JogoTerminal.java
+++ Atual/src/br/ufla/dcc/ppoo/trabalhofinal/interacaousuario/JogoTerminal.java
@@ -3,6 +3,7 @@ package br.ufla.dcc.ppoo.trabalhofinal.interacaousuario;
 import br.ufla.dcc.ppoo.trabalhofinal.regranegocio.RegraNegocio;
 import br.ufla.dcc.ppoo.trabalhofinal.comandos.Comando;
 import br.ufla.dcc.ppoo.trabalhofinal.comandos.Analisador;
+import java.util.Scanner;
 
 /**
  *
@@ -11,31 +12,32 @@ import br.ufla.dcc.ppoo.trabalhofinal.comandos.Analisador;
 public class JogoTerminal{
 
     /**
-     *
+     * Metodo principal da classe
      * @param args
      */
     public static void main(String[] args) {
+        //Variaveis
         RegraNegocio regraNegocio = new RegraNegocio();
         Analisador analisador = new Analisador();
+        Scanner entrada = new Scanner(System.in);         // origem da entrada de comandos
         Comando comando;
         String terminado = "ok"; // variavel que encerra o jogo
+        
+        //start do programa
         System.out.println(regraNegocio.mensagemBoasVindas());
         System.out.print("> ");
 
         // Entra no loop de comando principal. Aqui nos repetidamente lemos
         // comandos e os executamos ate o jogo terminar.
         while (terminado != null) {
-            comando = analisador.pegarComandoConsole();
-            if(regraNegocio.processarComando(comando) != null){
-                System.out.println(regraNegocio.processarComando(comando));
-                System.out.print("> ");
-            }else{
-                terminado = regraNegocio.processarComando(comando);
-            }
+            terminado = entrada.nextLine();
+            comando = analisador.pegarComando(terminado);
+            terminado = regraNegocio.processarComando(comando);
+            System.out.println(terminado);
+            System.out.print("> ");
         }
 
         //Retorna mensagem final
         System.out.println("\nObrigado por jogar. Ate mais!\n");
     }
-
 }

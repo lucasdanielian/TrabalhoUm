c90e8c1e54d12aba4cec9c67ffd0ff39bcfd6e55
vssjunior <valdecisoares23@yahoo.com.br>
2017-07-31 16:07:21 -0300

    Adicionando tela de Ranking de jogadores

--- Atual/src/br/ufla/dcc/gcc178/s2017_01/trabalho2/supernatural/gui/TelaRankingJogadores.java
+++ Atual/src/br/ufla/dcc/gcc178/s2017_01/trabalho2/supernatural/gui/TelaRankingJogadores.java
@@ -1,7 +1,16 @@
 package br.ufla.dcc.gcc178.s2017_01.trabalho2.supernatural.gui;
 
 import br.ufla.dcc.gcc178.s2017_01.trabalho2.supernatural.interacaousuario.TelaPrincipal;
-import javax.swing.JOptionPane;
+import br.ufla.dcc.gcc178.s2017_01.trabalho2.supernatural.i18n.I18N;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /**
  * Classe que representa a tela Meus Filmes
@@ -11,6 +20,19 @@ import javax.swing.JOptionPane;
 public class TelaRankingJogadores {
     
     private final TelaPrincipal telaPrincipal;
+            
+    // componentes da tela
+    private JDialog janela;
+    private GridBagLayout layout;
+    private GridBagConstraints gbc;
+    private JTable tbRankingJogadores;
+
+     /**
+ * Classe que representa a tela Meus Filmes
+ * 
+ * @author Julio Alves e Paulo Jr.
+ */
+
     
     public TelaRankingJogadores(TelaPrincipal telaPrincipal) {
         this.telaPrincipal = telaPrincipal;
@@ -22,6 +44,91 @@ public class TelaRankingJogadores {
      */
     public void inicializar(){
 
-        JOptionPane.showMessageDialog(null, "Ainda não foi emplementado o Ranking dos jogadores");
+        construirTela();
+        exibirTela();
+    }
+
+    /**
+     * Constrói a janela tratando internacionalização, componentes e layout.
+     */
+    private void construirTabela() {
+
+        Object[] titulosColunas = {
+            I18N.obterRotuloJogador(),
+            I18N.obterRotuloDiasCorridos()
+        };
+
+        // Dados "fake"
+        Object[][] dados = {{"Joao", "15"},
+            {"Maria", "25"}
+        };
+
+        tbRankingJogadores = new JTable(dados, titulosColunas);
+        tbRankingJogadores.setPreferredScrollableViewportSize(new Dimension(700, 500));
+        tbRankingJogadores.setFillsViewportHeight(true);
+    }
+
+    /**
+     * Adiciona um componente à tela.
+     */
+    private void adicionarComponente(Component c,
+            int anchor, int fill, int linha,
+            int coluna, int largura, int altura) {
+        gbc.anchor = anchor;
+        gbc.fill = fill;
+        gbc.gridy = linha;
+        gbc.gridx = coluna;
+        gbc.gridwidth = largura;
+        gbc.gridheight = altura;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        layout.setConstraints(c, gbc);
+        janela.add(c);
+    }
+
+    /**
+     * Trata o estado inicial da tela
+     */
+    private void prepararComponentesEstadoInicial() {
+        tbRankingJogadores.clearSelection();
+        tbRankingJogadores.setEnabled(true);
+
+    }
+
+    /**
+     * Adiciona os componentes da tela tratando layout e internacionalização
+     */
+    private void adicionarComponentes() {
+        construirTabela();
+        JScrollPane scrollPaneTabela = new JScrollPane(tbRankingJogadores);
+        adicionarComponente(scrollPaneTabela,
+                GridBagConstraints.CENTER,
+                GridBagConstraints.NONE,
+                0, 0, 4, 1);
+
+        prepararComponentesEstadoInicial();
+    }
+
+    /**
+     * Constrói a janela tratando internacionalização, componentes e layout.
+     */
+    private void construirTela() {
+        janela = new JDialog();
+        janela.setTitle(I18N.obterTituloTelaRankingJogadores());
+        layout = new GridBagLayout();
+        gbc = new GridBagConstraints();
+        janela.setLayout(layout);
+        adicionarComponentes();
+        janela.pack();
+    }
+
+    /**
+     * Exibe a tela.
+     */
+    private void exibirTela() {
+        janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        janela.setLocationRelativeTo(telaPrincipal.obterJanela());
+        janela.setModal(true);
+        janela.setVisible(true);
+        janela.setResizable(false);
     }
 }
